ae2e32b67d7baf162e274f0f5c66119ee132328f
caffeine110 <gaurav.developer.audio@gmail.com>
2018-09-10 23:59:15 +0530

    updates..

---
+++ random_forest/rf_author_id.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on  Fri  Aug 10 23:43:32 2018
+
+@author : gaurav gahukar
+        : caffeine110
+
+AIM     : (p) TO Impliment UDACITY MiniProject ( Random Forest )
+        : (s) Email Classification to predict Author of the Email
+
+        : --- Inspired by the story of J K Roling ---
+          The Sunday (UK) Times recently revealed that J.K. Rowling wrote the
+          detective novel The Cuckoo's Calling under the pen name Robert Galbraith.
+          After classification using classification algorithms it was confirmed that
+          J K Rolling wrote that book. 
+"""
+
+
+""" 
+    This is the code to accompany the Lesson 1 ( Random Forest) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+
+
+#importing required libraries
+import sys
+from time import time
+
+#Set the working directory
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+#import GaussianNB
+from sklearn.naive_bayes import GaussianNB
+
+#initialising classifier
+gnb_classifier = GaussianNB()
+
+#fitting data to the classifer
+gnb_classifier.fit(features_train, labels_train)
+
+
+
+from sklearn.metrics import accuracy_score
+#prediction
+author_pred = gnb_classifier.predict(features_test)
+
+
+#printing the accuracy
+print(accuracy_score(labels_test, author_pred))
+print(gnb_classifier.score(features_test, labels_test))
